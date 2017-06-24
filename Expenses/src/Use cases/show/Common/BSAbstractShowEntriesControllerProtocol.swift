10a0e24e95202554f6267a3267e0c4237a5b748a
Borja Arias Drake <borja.arias@gmail.com>
2017-06-24 07:00:16 +0100

    - Add comments to BSAbstractShowEntriesControllerProtocol

--- Expenses/src/Use cases/show/Common/BSAbstractShowEntriesControllerProtocol.swift
+++ Expenses/src/Use cases/show/Common/BSAbstractShowEntriesControllerProtocol.swift
@@ -8,11 +8,38 @@
 
 import Foundation
 
+
+/// Summary screens' controller objects have a number of characteristics collected in this protocol.
 @objc protocol BSAbstractShowEntriesControllerProtocol : BSCoreDataControllerProtocol {
     
-    func filter(by category : Tag) // Just changes internal configuration to filter next time entries for summary gets called
+    /// Changes internal configuration to filter accordanly next time entries for summary gets called.
+    ///
+    /// - Parameter category: The category to filter by.
+    func filter(by category : Tag)
+    
+    
+    /// Fetches a collection of entries, groupped by sectionNameKeyPath
+    ///
+    /// - Returns: A dictionary containing the results groupped by sectionNameKeyPath.
     func entriesForSummary() -> NSDictionary
+    
+    
+    /// Fetches an image for a given category
+    ///
+    /// - Parameter category: The category to search an image for.
+    /// - Returns: An image. Nil if there was an error.
     func image(for category :Tag?) -> UIImage?
+    
+    
+    /// A string descriptor for the sections in the fetched results.
+    /// This descriptor is currently coupled with CoreData. It corresponds with a field
+    /// in the entities, to be able to effectively group by. However, this is an implementation detail
+    /// that users of the controllers don't need to understand.
+    ///
+    /// - Returns: Descriptor of a section in the summary result.
     func sectionNameKeyPath() -> String?
+    
+    
+    /// TODO: Do not expose this.
     var _fetchedResultsController: NSFetchedResultsController<NSFetchRequestResult>? {get}
 }

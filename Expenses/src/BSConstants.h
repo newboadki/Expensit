65729d553a566666c68e7bb28956dcd849342269
Borja Arias Drake <borja.arias@gmail.com>
2018-10-20 21:00:03 +0200

    Removing ViewControllers folder and moving files into corresponding subfolders.

--- Expenses/src/BSConstants.h
+++ Expenses/src/BSConstants.h
@@ -11,13 +11,3 @@
 // FILES
 static const NSString * FIRSTTIME_FILE = @"firstTimeFile";
 
-// ENTRY TYPE
-static const NSInteger EXPENSES_SEGMENTED_INDEX = 0;
-static const NSInteger BENEFITS_SEGMENTED_INDEX = 1;
-
-// CATEGORIES
-static const NSInteger BILLS_SEGMENTED_INDEX = 0;
-static const NSInteger FOOD_SEGMENTED_INDEX = 1;
-static const NSInteger CLOTHES_SEGMENTED_INDEX = 2;
-static const NSInteger TRAVEL_SEGMENTED_INDEX = 3;
-static const NSInteger OTHER_SEGMENTED_INDEX = 4;

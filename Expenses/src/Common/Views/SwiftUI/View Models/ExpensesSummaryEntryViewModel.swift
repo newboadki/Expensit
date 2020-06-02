d3757552f826e840c88f1119085f75ea2e9e9357
Borja Arias Drake <borja.arias@gmail.com>
2020-06-02 08:43:08 +0200

    Grouped files under folders.

--- Expenses/src/Common/Views/SwiftUI/View Models/ExpensesSummaryEntryViewModel.swift
+++ Expenses/src/Common/Views/SwiftUI/View Models/ExpensesSummaryEntryViewModel.swift
@@ -1,29 +1,12 @@
 //
-//  SummarySection.swift
+//  ExpensesSummaryEntryViewModel.swift
 //  Expensit
 //
-//  Created by Borja Arias Drake on 16/10/2019.
-//  Copyright © 2019 Borja Arias Drake. All rights reserved.
+//  Created by Borja Arias Drake on 01/06/2020.
+//  Copyright © 2020 Borja Arias Drake. All rights reserved.
 //
 
 import Foundation
-
-
-class ExpensesSummarySectionViewModel: Identifiable
-{
-    var id: Int
-    let title : String?
-    let entries : [ExpensesSummaryEntryViewModel]
-    var numberOfEntries : Int  { get { return entries.count } }
-    
-    init(id: Int, title : String?, entries : [ExpensesSummaryEntryViewModel])
-    {
-        self.id = id
-        self.title = title
-        self.entries =  entries
-    }
-}
-
 
 struct ExpensesSummaryEntryViewModel : Identifiable
 {

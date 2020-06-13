d3b499d13ffecd94dca15f90fc3229c2875b7138
Borja Arias Drake <borja.arias@gmail.com>
2020-06-13 14:20:11 +0200

    Split NavigationButtons' subviews into separate views.

---
+++ Expenses/src/Use cases/Show/Common/Views/Navigation Buttons/AddEntryButton.swift
@@ -0,0 +1,33 @@
+//
+//  AddEntryButton.swift
+//  Expensit
+//
+//  Created by Borja Arias Drake on 13/06/2020.
+//  Copyright © 2020 Borja Arias Drake. All rights reserved.
+//
+
+import SwiftUI
+
+struct AddEntryButton: View {
+    // MARK: Private
+    private var entryFormCoordinator: EntryFormNavigationCoordinator
+    @State private var showEntryForm = false
+
+    // MARK: Initializers
+    init(entryFormCoordinator: EntryFormNavigationCoordinator) {
+        self.entryFormCoordinator = entryFormCoordinator
+    }
+    
+    var body: some View {
+        Button(action: {
+            self.showEntryForm.toggle()
+        }) {
+            Image(systemName: "plus")
+        }.sheet(isPresented: $showEntryForm) {
+            self.entryFormCoordinator.entryFormView(forIdentifier:"", isPresented: self.$showEntryForm)
+        }
+    }
+}
+
+
+
---
+++ Expenses/src/Use cases/Show/Common/Views/Navigation Buttons/CategoryFilterButtonView.swift
@@ -0,0 +1,32 @@
+//
+//  CategoryFilterButtonView.swift
+//  Expensit
+//
+//  Created by Borja Arias Drake on 13/06/2020.
+//  Copyright © 2020 Borja Arias Drake. All rights reserved.
+//
+
+import SwiftUI
+
+struct CategoryFilterButtonView: View {
+    // MARK: Private
+    private var categoryFilterNavgationCoordinator: CategoryFilterNavigationCoordinator
+    @ObservedObject private var presenter: NavigationButtonsPresenter
+    @State private var showCategoryFilter = false
+
+    init(categoryFilterNavgationCoordinator: CategoryFilterNavigationCoordinator,
+         presenter: NavigationButtonsPresenter) {
+        self.categoryFilterNavgationCoordinator = categoryFilterNavgationCoordinator
+        self.presenter = presenter
+    }
+    
+    var body: some View {
+        Button(action: {
+            self.showCategoryFilter.toggle()
+        }) {
+            Image(self.presenter.selectedCategoryImageName)
+        }.sheet(isPresented: $showCategoryFilter) {
+            self.categoryFilterNavgationCoordinator.categoryFilterView(forIdentifier:"", isPresented: self.$showCategoryFilter)
+        }
+    }
+}

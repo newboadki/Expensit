8d4da651f50cd733e82e233103dbc24aa4991a1a
Borja Arias Drake <borja.arias@gmail.com>
2020-06-21 08:13:59 +0200

    Fixing test target. Add initial test.

--- ExpensesTests/YearlySummaryTests.swift
+++ ExpensesTests/YearlySummaryTests.swift
@@ -9,29 +9,25 @@
 import XCTest
 
 class YearlySummaryTests: XCTestCase {
-    private var yearlyVC: BSYearlyExpensesSummaryViewController!
+    
+    private var presenter: ShowYearlyEntriesPresenter!
     
     override func setUp() {
-        TestDataGenerator().generate()
-        
-        let storyboard = UIStoryboard.init(name: "Storyboard", bundle: nil)
-        self.yearlyVC = storyboard.instantiateViewController(identifier: "BSYearlyExpensesSummaryViewController") as! BSYearlyExpensesSummaryViewController
-
-        let coreDataController = TestCoreDataStackHelper.coreDataController()        
-        let fetchController = BSCoreDataFetchController(coreDataController: coreDataController)
-        let controller = BSShowYearlyEntriesController(dataProvider: fetchController)
-        let presenter = BSShowYearlyEntriesPresenter(showEntriesUserInterface: yearlyVC, showEntriesController: controller)
-        yearlyVC.showEntriesPresenter = presenter;
+        var tg = TestDataGenerator()
+        tg.generate()
+                
+        let yearlySummaryDataSource = YearlyCoreDataExpensesDataSource(coreDataController:tg.coreDataController,
+                                                                       selectedCategoryDataSource: SelectedCategoryDataSource())
+        presenter = ShowYearlyEntriesPresenter(interactor: ExpensesSummaryInteractor(dataSource: yearlySummaryDataSource))
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        
     }
 
     func testExample() {
-        yearlyVC.showEntriesPresenter?.viewIsReadyToDisplayEntriesCompletionBlock({ section in
-            
-        })
+        XCTAssert(presenter.sections.count == 1, "There should be one section.")
+        XCTAssert(presenter.sections.first!.entries.count == 3, "There should be x years.")
     }
 
     

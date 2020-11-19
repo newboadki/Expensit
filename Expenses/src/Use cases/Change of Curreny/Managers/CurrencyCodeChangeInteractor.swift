2c17435b80a9e73c7600158d78eeeef7cde5efb6
Borja Arias Drake <borja.arias@gmail.com>
2020-11-19 12:26:24 +0100

    Change currency code change algorithm.

---
+++ Expenses/src/Use cases/Change of Curreny/Managers/CurrencyCodeChangeInteractor.swift
@@ -0,0 +1,70 @@
+//
+//  File.swift
+//  
+//
+//  Created by Borja Arias Drake on 19.11.2020..
+//
+
+import Foundation
+import Currencies
+import CoreExpenses
+import CoreDataPersistence
+
+class CurrencyCodeChangeInteractor {
+    
+    private var exchangeRatesConversionInteractor: ConvertToBaseCurrencyInteractor
+    private var allEntriesDataSource: AllEntriesCoreDataExpensesDataSource
+    private var currencySettingsInteractor: CurrencySettingsInteractor
+    
+    init(exchangeRatesConversionInteractor: ConvertToBaseCurrencyInteractor,
+         allEntriesDataSource: AllEntriesCoreDataExpensesDataSource,
+         currencySettingsInteractor: CurrencySettingsInteractor) {
+        self.exchangeRatesConversionInteractor = exchangeRatesConversionInteractor
+        self.allEntriesDataSource = allEntriesDataSource
+        self.currencySettingsInteractor = currencySettingsInteractor
+    }
+            
+    func updateCurrencyExchangeRatesIfNeeded() {
+        let previousCode = currencySettingsInteractor.previousCurrencyCode()
+        let currentCode = currencySettingsInteractor.currentCurrencyCode()
+        let currencyCodeChanged = (previousCode != currentCode)
+        if currencyCodeChanged {
+            // Convert
+            let approximated = convertExchangeRates(from: previousCode, to: currentCode)
+            
+            // Update state if needed
+            if !approximated {
+                currencySettingsInteractor.setPreviousCurrencyCode(currentCode)
+            }
+        } else {
+            converExchangeRatesIfCalculationsAreApproximated()
+        }
+    }
+    
+    private func convertExchangeRates(from: String, to: String) -> Bool {
+        exchangeRatesConversionInteractor.convertAllEntries(from: from, to: to)
+        return allEntriesDataSource.isExchangeRateToBaseApproximated()
+    }
+    
+    private func converExchangeRatesIfCalculationsAreApproximated() {
+        // Check that we need to recalculate
+        guard allEntriesDataSource.isExchangeRateToBaseApproximated() else {
+            return
+        }
+        
+        // Check the state is not corrupt
+        let previousCode = currencySettingsInteractor.previousCurrencyCode()
+        let currentCode = currencySettingsInteractor.currentCurrencyCode()
+        guard (previousCode != currentCode) else {
+            fatalError("Currency rates can't be approximate if current and previous codes are the same.")
+        }
+        
+        // Convert
+        let approximated = convertExchangeRates(from: previousCode, to: currentCode)
+        
+        // Update state if needed
+        if !approximated {
+            currencySettingsInteractor.setPreviousCurrencyCode(currentCode)
+        }
+    }
+}
